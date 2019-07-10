1982ee7258bb6eee442e450aae1d1d8aaf5f5c0c
ALVARLEUNG <alvar.leung@qq.com>
2019-07-10 18:16:56 +0800

    practice: finished the map function

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,14 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+  return collection.map((item)=>item*2);
 }
+
+Array.prototype.getCollection = function (prop) {
+  let result = [];
+  for (let i =0; i<this.length; i++) {
+          result.push(porp(this[i]));
+      }
+      return result;
+  }
+
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,13 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  return collection.map((item)=>item*4+1);
 };
+Array.prototype.getFourMultiplesAddOne = function (prop) {
+  let result = [];
+  for (let i =0; i<this.length; i++) {
+          result.push(porp(this[i]));
+      }
+      return result;
+  }
 
 module.exports = map_to_four_multiples_add_one;
