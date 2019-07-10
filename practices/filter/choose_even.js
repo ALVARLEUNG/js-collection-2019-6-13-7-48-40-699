d4a52ac65fc0ac6bec6b62ed8a85ec5fd617842a
ALVARLEUNG <alvar.leung@qq.com>
2019-07-10 15:51:45 +0800

    practice: finish functions

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,21 @@
 function choose_even(collection) {
 
   //implement here
+  // return collection.chooseMultiples();
+  const chooseMultiples = (a) => {
+    return a % 2 === 0;
+  }
+  return collection.filter(chooseMultiples);
+
 }
 
+// Array.prototype.chooseMultiples = function () {
+//   let result = [];
+//   for(let i=0; i<this.length; i++) {
+//     if (this[i] % 2 === 0) {
+//       result.push(this[i]);
+//     }
+//   }
+//   return result;
+// }
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,21 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  // return collection.chooseMultiples();
+  const chooseMultiplesThree = (a) => {
+    return a % 3 === 0;
+  }
+  return collection.filter(chooseMultiplesThree);
 }
+
+// Array.prototype.chooseMultiples = function () {
+//   let result = [];
+//   for(let i=0; i<this.length; i++) {
+//     if (this[i] % 3 === 0) {
+//       result.push(this[i]);
+//     }
+//   }
+//   return result;
+// }
 
 module.exports = choose_multiples_of_three;
